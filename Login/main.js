95fe9a9e05a037764f3a4b091593e0e7ac40dd91
Gabriel Portaluppi <111152789+GabrielPortaluppi@users.noreply.github.com>
2023-02-13 20:46:07 -0300

    saida e entrada

---
+++ Login/main.js
@@ -0,0 +1,19 @@
+const botao = document.querySelector('#botao');
+botao.addEventListener("click", clicar)
+
+function login(email, senha){
+   alert(email, senha) 
+}
+
+function clicar(){
+    let email = document.querySelector('#iemail');
+    let ema = String(email.value)
+    let senha = document.querySelector('#isenha');
+    let sen = String(senha.value)
+
+    let pessoa = (Acessou)
+    if(ema == "gabriel@gmail.com"){
+        if(sen == "gabriel11"){
+            alert(pessoa)
+    }   }
+}
